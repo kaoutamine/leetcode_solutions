0937adb6f08486b58fb881415dc44ee36477c829
kaoutamine <kaourintin.tamine@jobcloud.ch>
2025-03-25 15:10:22 +0100

    solution with hashmap

--- 1_2SUM.py
+++ 1_2SUM.py
@@ -29,3 +29,16 @@ class Solution:
             total = pairs[p1][1] + pairs[p2][1]
         
         return pairs[p1][0], pairs[p2][0]
+    
+
+"""Solution with hashmap"""
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        
+        for i, num in enumerate(nums):
+            search = target - num
+            if search in seen:
+                return i, seen[search]
+            else:
+                seen[num] = i
