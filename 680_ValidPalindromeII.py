85067ba6580af4219bc21dee167400786e8f8b65
kaoutamine <kaourintin.tamine@jobcloud.ch>
2025-04-15 20:56:59 +0200

    new solution of valid palindrome 2

--- 680_ValidPalindromeII.py
+++ 680_ValidPalindromeII.py
@@ -21,6 +21,38 @@ class Solution:
         return validity
 
 
+#another solution (I failed to redo it a second time :))))))))))))))))) 
+class Solution:
+    def checkPalindrome(self, i, j, s):
+        while i < j:
+            if s[i] != s[j]:
+                return False, i, j
+            i += 1
+            j -= 1
+        return True, i, j
+        
+
+    def validPalindrome(self, s: str) -> bool:
+        validity, i, j = self.checkPalindrome(0, len(s) - 1, s)
+        if validity == True:
+            return True
+
+        validity, _, _ = self.checkPalindrome(i + 1, j, s)
+        if validity == True:
+            return True
+        validity, _, _ = self.checkPalindrome(i, j - 1, s)
+        
+        return validity
+
+
+        
+                
+                
+
+
+
+        
+
         
                 
                 
